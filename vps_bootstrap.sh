be07e720c470e16c1b50be45473e34eae48cfc8b
Gregory Meno <gregory.meno@inktank.com>
2015-03-17 11:17:09 -0400

    devmode: pin to a specific version of salt
    Updated docs to reflect the new process
    
    Signed-off-by: Gregory Meno <gregory.meno@inktank.com>

---
+++ vps_bootstrap.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+set -e
+
+wget -O install_salt.sh https://bootstrap.saltstack.com
+sudo sh install_salt.sh -P git v2014.1.13
+pushd /calamari.git
+sudo salt-call --local --file-root=$(pwd)/vagrant/devmode/salt/roots state.highstate
+popd
+
